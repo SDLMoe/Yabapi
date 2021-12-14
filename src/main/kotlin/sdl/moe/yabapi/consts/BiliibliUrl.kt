f229c153e48827b8d4882cd5747d39009a60e1e1
Colerar <233hbj@gmail.com>
2021-12-14 15:00:11 +0800

    Add Api Url Consts

---
+++ src/main/kotlin/sdl/moe/yabapi/consts/BiliibliUrl.kt
@@ -0,0 +1,38 @@
+@file:Suppress("UNUSED")
+
+package sdl.moe.yabapi.consts
+
+/**
+ * 主站
+ */
+const val MAIN = "https://api.bilibili.com"
+
+/**
+ * 手机端API
+ */
+const val APP = "https://app.bilibili.com"
+
+/**
+ * 消息中心
+ */
+const val MESSAGE = "https://message.bilibili.com"
+
+/**
+ * 直播中心
+ */
+const val LIVE = "https://live.bilibili.com"
+
+/**
+ * 创作中心
+ */
+const val MEMBER = "https://member.bilibili.com"
+
+/**
+ * 鉴权
+ */
+const val PASSPORT = "https://passport.bilibili.com"
+
+/**
+ * 短视频
+ */
+const val VC = "https://vc.bilibili.com"
