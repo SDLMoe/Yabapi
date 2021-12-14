f73691a8589c94f0f27e46cd84eac4b63bcbab2c
Colerar <233hbj@gmail.com>
2021-12-14 16:35:34 +0800

    Add ImageUrlBuilder.kt

---
+++ src/main/kotlin/sdl/moe/yabapi/enums/ImageFormat.kt
@@ -0,0 +1,11 @@
+@file:Suppress("UNUSED")
+
+package sdl.moe.yabapi.enums
+
+enum class ImageFormat {
+    UNKNOWN,
+    PNG,
+    JPEG,
+    GIF,
+    WEBP;
+}
---
+++ src/test/kotlin/sdl/moe/yabapi/util/ImageUrlBuilder.kt
@@ -0,0 +1,14 @@
+package sdl.moe.yabapi.util
+
+import org.junit.jupiter.api.Test
+import sdl.moe.yabapi.enums.ImageFormat
+
+class ImageUrlBuilder {
+    @Test
+    fun main() {
+        val url = "https://i1.hdslb.com/bfs/archive/e5fff1472bad1c0c6bcb3004205f9be23b58ffc0.jpg@1500w_1500h_75q.webp"
+        buildImageUrl(url, ImageFormat.JPEG).also(::println)
+        buildImageUrl(url, ImageFormat.JPEG, 75, 114, 514).also(::println)
+        buildImageUrl(url, ImageFormat.WEBP, 75, 114, 514).also(::println)
+    }
+}
