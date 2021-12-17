674507d4ba5ef590c6832ddcbaa1bb1d71ba40ee
Colerar <233hbj@gmail.com>
2021-12-18 00:53:05 +0800

    Reformat with compact doc style.

--- src/main/kotlin/sdl/moe/yabapi/consts/BiliUrl.kt
+++ src/main/kotlin/sdl/moe/yabapi/consts/BiliUrl.kt
@@ -2,39 +2,25 @@
 
 package sdl.moe.yabapi.consts
 
-/**
- * 主站
- */
+/** 主站 */
 internal const val MAIN: String = "https://api.bilibili.com"
 
-/**
- * 手机端API
- */
+/** 手机端API */
 internal const val APP: String = "https://app.bilibili.com"
 
-/**
- * 消息中心
- */
+/** 消息中心 */
 internal const val MESSAGE: String = "https://message.bilibili.com"
 
-/**
- * 直播中心
- */
+/** 直播中心 */
 internal const val LIVE: String = "https://live.bilibili.com"
 
-/**
- * 创作中心
- */
+/** 创作中心 */
 internal const val MEMBER: String = "https://member.bilibili.com"
 
-/**
- * 鉴权
- */
+/** 鉴权 */
 internal const val PASSPORT: String = "https://passport.bilibili.com"
 
-/**
- * 短视频
- */
+/** 短视频 */
 internal const val VC: String = "https://vc.bilibili.com"
 
 internal const val INTERFACE = "https://interface.bilibili.com"
