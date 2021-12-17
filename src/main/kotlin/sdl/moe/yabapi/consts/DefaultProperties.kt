c9348d76ed585525d50df6f189484e27f5842ae8
Colerar <233hbj@gmail.com>
2021-12-17 15:18:21 +0800

    Remove install cookies in DefaultProperties.kt

--- src/main/kotlin/sdl/moe/yabapi/consts/DefaultProperties.kt
+++ src/main/kotlin/sdl/moe/yabapi/consts/DefaultProperties.kt
@@ -5,26 +5,15 @@ package sdl.moe.yabapi.consts
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.features.UserAgent
-import io.ktor.client.features.cookies.AcceptAllCookiesStorage
-import io.ktor.client.features.cookies.HttpCookies
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.features.websocket.WebSockets
-import io.ktor.http.Cookie
-import io.ktor.http.Url
 
 public val DefaultHttpClient: HttpClient by lazy {
     HttpClient(CIO) {
         install(WebSockets)
         install(UserAgent) {
             agent = WEB_USER_AGENT
-        }
-        install(HttpCookies) {
-            // Will keep an in-memory map with all the cookies from previous requests.
-            storage = AcceptAllCookiesStorage()
-            this.default {
-                this.addCookie(Url("bilibili.com"), Cookie("lang", "zh-cn"))
-            }
         }
         install(JsonFeature) {
             val json = kotlinx.serialization.json.Json {
