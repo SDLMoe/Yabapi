e043546e18ef687f082438ef5d760fcfa13cd942
Colerar <233hbj@gmail.com>
2021-12-18 17:51:30 +0800

    Delete detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ val ktlint: Configuration by configurations.creating
 
 plugins {
     kotlin("jvm") version "1.6.0"
-    id("io.gitlab.arturbosch.detekt").version("1.19.0")
     kotlin("plugin.serialization") version "1.6.0"
 }
 
@@ -84,31 +83,7 @@ val ktlintFormat by tasks.creating(JavaExec::class) {
     args = listOf("-F", "src/**/*.kt")
 }
 
-detekt {
-    buildUponDefaultConfig = true // preconfigure defaults
-    allRules = false // activate all available (even unstable) rules.
-    config =
-        files("$projectDir/config/detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
-    baseline = file("$projectDir/config/baseline.xml") // a way of suppressing issues before introducing detekt
-}
-
-tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
-    reports {
-        html.required.set(true) // observe findings in your browser with structure and code snippets
-        xml.required.set(true) // checkstyle like format mainly for integrations like Jenkins
-        txt.required.set(true) // similar to the console output, contains issue signature to manually edit baseline files
-        sarif.required.set(true) // standardized SARIF format (https://sarifweb.azurewebsites.net/) to support integrations with GitHub Code Scanning
-    }
-}
-
 kotlin {
     explicitApi()
     explicitApiWarning()
-}
-
-tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
-    jvmTarget = "11"
-}
-tasks.withType<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>().configureEach {
-    jvmTarget = "11"
 }
