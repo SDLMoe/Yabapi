76bd834bdaf2bb7a02493bea46bc23fb37391277
Colerar <233hbj@gmail.com>
2021-12-18 15:25:52 +0800

    Add SendSMSResponse.kt and its Serializer

---
+++ src/main/kotlin/sdl/moe/yabapi/data/login/SendSMSResponse.kt
@@ -0,0 +1,75 @@
+// © Copyright 2021 Colerar and repository contributors
+// Use of this source code is governed by the MIT license that can be found via link below:
+// https://github.com/SDLMoe/Yabapi/blob/master/LICENSE
+
+package sdl.moe.yabapi.data.login
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import sdl.moe.yabapi.serializer.data.login.SendSMSResponseCodeSerializer
+
+/**
+ * 發送短信驗證碼返回
+ * @param code 狀態碼 [SendSMSResponseCode]
+ * @param message 錯誤碼, 成功爲 "0"
+ * @param data [SendSMSResponseData]
+ * @property captchaKey 封裝, [SendSMSResponseData.captchaKey]
+ */
+@Serializable
+public data class SendSMSResponse(
+    val code: SendSMSResponseCode,
+    val message: String,
+    val data: SendSMSResponseData,
+) {
+    public val captchaKey: String
+        get() = data.captchaKey
+}
+
+/**
+ * 發送短信驗證碼返回狀態碼
+ * @property UNKNOWN 未知
+ * @property SUCCESS 成功
+ * @property ERROR_REQUEST 請求錯誤
+ * @property INVALID_PHONE 手機格式錯誤
+ * @property HAS_SENT 已發送過(需等待CD)
+ * @property BANNED 手機號碼被封禁
+ * @property INVALID_LOGIN_KEY 登入碼錯誤
+ * @property CAPTCHA_SERVICE_ERROR 驗證碼服務錯誤
+ * @property REQUEST_TOO_FREQUENT 請求過於頻繁
+ */
+@Serializable(with = SendSMSResponseCodeSerializer::class)
+public enum class SendSMSResponseCode {
+    UNKNOWN,
+
+    @SerialName("0")
+    SUCCESS,
+
+    @SerialName("-400")
+    ERROR_REQUEST,
+
+    @SerialName("1002")
+    INVALID_PHONE,
+
+    @SerialName("1003")
+    HAS_SENT,
+
+    @SerialName("1025")
+    BANNED,
+
+    @SerialName("2400")
+    INVALID_LOGIN_KEY,
+
+    @SerialName("2406")
+    CAPTCHA_SERVICE_ERROR,
+
+    @SerialName("86203")
+    REQUEST_TOO_FREQUENT;
+}
+
+/**
+ * @param captchaKey 验证密钥
+ */
+@Serializable
+public data class SendSMSResponseData(
+    @SerialName("captcha_key") val captchaKey: String
+)
