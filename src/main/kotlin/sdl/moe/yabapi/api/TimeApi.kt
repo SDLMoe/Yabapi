af99fe4e7e8558c72532eec5da127687828476a0
Colerar <233hbj@gmail.com>
2021-12-20 00:51:48 +0800

    Delete TimeApi getServerUTC func

--- src/main/kotlin/sdl/moe/yabapi/api/TimeApi.kt
+++ src/main/kotlin/sdl/moe/yabapi/api/TimeApi.kt
@@ -9,15 +9,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import mu.KotlinLogging
 import sdl.moe.yabapi.BiliClient
-import sdl.moe.yabapi.consts.time.GET_SERVER_UTC_URL
 import sdl.moe.yabapi.consts.time.GET_TIMESTAMP_URL
 import sdl.moe.yabapi.data.time.TimestampGetResponse
-import java.time.ZoneId
-import java.time.ZonedDateTime
 
 private val logger = KotlinLogging.logger {}
-
-private val jsUTCRegex by lazy { Regex("""UTC\((\d+),\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+)\)""") }
 
 public object TimeApi : BiliApi {
     init {
@@ -44,36 +39,4 @@ public object TimeApi : BiliApi {
 
     @JvmName("getTimestamp")
     public suspend inline fun getTimestamp(client: BiliClient): TimestampGetResponse = client.getTimestamp()
-
-    /**
-     * 獲取服務端側 UTC 時間
-     * @return [ZonedDateTime]
-     */
-    @Suppress("MagicNumber")
-    @JvmName("getServerUTCExt")
-    public suspend fun BiliClient.getServerUTC(): ZonedDateTime = withContext(Dispatchers.IO) {
-        logger.debug { "Getting UTC" }
-        val string = client.get<String>(GET_SERVER_UTC_URL).also {
-            logger.debug { "UTC Get Response: $it" }
-        }
-
-        fun throwFailed(): Nothing = throw IllegalArgumentException("Failed to parse UTC, rawString: '$string'")
-        val values = jsUTCRegex.find(string)?.groupValues
-            ?.drop(1)
-            ?.fold(arrayListOf<Int>()) { acc, s ->
-                val value = s.toIntOrNull() ?: throw IllegalArgumentException("Failed to parse value: $s")
-                acc.add(value)
-                acc
-            } ?: throwFailed()
-        require(values.size == 6) { throwFailed() }
-        val utcDate =
-            ZonedDateTime.of(values[0], values[1], values[2], values[3], values[4], values[5], 0, ZoneId.of("UTC"))
-                ?: throwFailed()
-        utcDate.also {
-            logger.debug { "UTC Parsed: $it" }
-        }
-    }
-
-    @JvmName("getServerUTC")
-    public suspend fun getServerUTC(client: BiliClient): ZonedDateTime = client.getServerUTC()
 }
