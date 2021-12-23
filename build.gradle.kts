43b4fadab5cfc3d164dcc742affd7fe3a67520ee
Colerar <233hbj@gmail.com>
2021-12-24 00:32:42 +0800

    Cleanup dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,19 +18,11 @@ repositories {
 }
 
 dependencies {
-    // Code Formatter
-    ktlint("com.pinterest:ktlint:0.43.2") {
-        attributes {
-            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
-        }
-    }
     // Test
     val jupiterVersion = "5.8.2"
     implementation("org.junit.jupiter:junit-jupiter:$jupiterVersion")
     testImplementation("org.junit.jupiter:junit-jupiter:$jupiterVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.0")
-    // Reflect
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.0")
     // Kotlinx Libraries
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
@@ -49,8 +41,6 @@ dependencies {
     implementation("io.ktor:ktor-client-websockets:$ktorVersion")
     // QR Code
     implementation("com.github.kenglxn.QRGen:javase:2.6.0")
-    // Encryption
-    // implementation("org.bouncycastle:bcprov-jdk15on:1.70")
 }
 
 tasks.withType<Test> {
@@ -59,30 +49,6 @@ tasks.withType<Test> {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
-    kotlinOptions.freeCompilerArgs += "-Xopt-in=org.mylibrary.OptInAnnotation"
-}
-
-val outputDir = "${project.buildDir}/reports/ktlint/"
-val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
-
-val ktlintCheck by tasks.creating(JavaExec::class) {
-    inputs.files(inputFiles)
-    outputs.dir(outputDir)
-
-    description = "Check Kotlin code style."
-    classpath = ktlint
-    main = "com.pinterest.ktlint.Main"
-    args = listOf("src/**/*.kt")
-}
-
-val ktlintFormat by tasks.creating(JavaExec::class) {
-    inputs.files(inputFiles)
-    outputs.dir(outputDir)
-
-    description = "Fix Kotlin code style deviations."
-    classpath = ktlint
-    main = "com.pinterest.ktlint.Main"
-    args = listOf("-F", "src/**/*.kt")
 }
 
 kotlin {
