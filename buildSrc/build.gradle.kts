38238dabd45067dd3e5ec281933ace14a2fdcf74
Colerar <233hbj@gmail.com>
2021-12-24 16:27:06 +0800

    configure kotlin multiplatform project

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,24 @@
+// © Copyright 2021 Colerar and repository contributors
+// Use of this source code is governed by the MIT license that can be found via link below:
+// https://github.com/SDLMoe/Yabapi/blob/master/LICENSE
+
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+}
+
+val kotlinVersion = FileInputStream(file("../gradle.properties")).use { propFile ->
+    val ver = Properties().apply { load(propFile) }["kotlin.version"]
+    require(ver is String) { "kotlin.version must be string in ../gradle.properties, got $ver instead" }
+    ver
+}
+
+dependencies {
+    implementation(kotlin("gradle-plugin", kotlinVersion))
+}
---
+++ yabapi-core/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    kotlin("multiplatform")
+    kotlin("plugin.serialization") version Versions.kotlin
+}
+
+kotlin {
+
+    explicitApi()
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    js(BOTH) {
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                // Kotlinx Libraries
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.1")
+            }
+
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val jsMain by getting
+        val jsTest by getting
+        val nativeMain by getting
+        val nativeTest by getting
+    }
+}
