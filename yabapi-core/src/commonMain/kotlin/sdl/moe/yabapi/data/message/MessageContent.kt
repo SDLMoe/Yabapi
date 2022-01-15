c241243397c4aa9ee869163b623e4893130e2ce7
Colerar <233hbj@gmail.com>
2022-01-15 18:47:05 +0800

    remove MessageType, combine to MessageContent

--- yabapi-core/src/commonMain/kotlin/sdl/moe/yabapi/data/message/MessageContent.kt
+++ yabapi-core/src/commonMain/kotlin/sdl/moe/yabapi/data/message/MessageContent.kt
@@ -7,6 +7,7 @@ package sdl.moe.yabapi.data.message
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
@@ -15,20 +16,29 @@ import kotlinx.serialization.encoding.Encoder
 
 @Serializable
 public sealed class MessageContent {
+
+    public abstract val code: Int
+
     @Serializable
     public data class Text(
         @SerialName("content") val content: String,
-    ) : MessageContent()
+    ) : MessageContent() {
+        @Transient override val code: Int = 1
+    }
 
     @Serializable
     public data class Image(
         @SerialName("url") val url: String,
-    ) : MessageContent()
+    ) : MessageContent() {
+        @Transient override val code: Int = 2
+    }
 
     @Serializable
     public data class Recall(
         public val key: String,
     ) : MessageContent() {
+        @Transient override val code: Int = 5
+
         public companion object : KSerializer<Recall> {
             override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("MessageRecall", PrimitiveKind.STRING)
             override fun serialize(encoder: Encoder, value: Recall): Unit = encoder.encodeString(value.key)
--- yabapi-core/src/commonMain/kotlin/sdl/moe/yabapi/data/message/MessageData.kt
+++ yabapi-core/src/commonMain/kotlin/sdl/moe/yabapi/data/message/MessageData.kt
@@ -9,9 +9,6 @@ import kotlinx.datetime.Clock
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import sdl.moe.yabapi.data.message.MessageType.IMAGE
-import sdl.moe.yabapi.data.message.MessageType.RECALL
-import sdl.moe.yabapi.data.message.MessageType.TEXT
 
 @Serializable
 public data class MessageData(
@@ -21,14 +18,7 @@ public data class MessageData(
     val receiverType: Int = 1,
     val devId: String = generateFakeUUID(),
     val timestamp: Long = Clock.System.now().epochSeconds,
-) {
-    val msgType: MessageType = when (content) {
-        is MessageContent.Text -> TEXT
-        is MessageContent.Image -> IMAGE
-        is MessageContent.Recall -> RECALL
-    }
-}
-
+)
 public fun MessageData.put(
     builder: ParametersBuilder,
     json: Json = Json,
@@ -36,7 +26,7 @@ public fun MessageData.put(
     append("msg[sender_uid]", senderUid.toString())
     append("msg[receiver_id]", receiverId.toString())
     append("msg[receiver_type]", receiverType.toString())
-    append("msg[msg_type]", msgType.code.toString())
+    append("msg[msg_type]", content.code.toString())
     append("msg[dev_id]", devId)
     append("msg[timestamp]", timestamp.toString())
     append("msg[content]", json.encodeToString(content))
