5eb987116604af714bb2bacff4601e01e8de6ad5
Colerar <233hbj@gmail.com>
2022-01-27 12:41:07 +0800

    Add Video Attribute

---
+++ yabapi-core/src/commonMain/kotlin/sdl/moe/yabapi/data/video/VideoAttribute.kt
@@ -0,0 +1,67 @@
+package sdl.moe.yabapi.data.video
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+public data class VideoAttribute(
+    val noRanking: Boolean = false,
+    val noFeed: Boolean = false,
+    val noWeb: Boolean = false,
+    val noApp: Boolean = false,
+    val noSearch: Boolean = false,
+    val noRecommend: Boolean = false,
+    val isPgc: Boolean = false,
+    val notReportedAd: Boolean = false, // 私單廣告
+    val isAreaLimited: Boolean = false,
+    val blockAddTags: Boolean = false,
+) {
+    public fun encode(): Int = listOf(
+        noRanking to 0,
+        noFeed to 1,
+        noWeb to 2,
+        noApp to 3,
+        noSearch to 4,
+        noRecommend to 6,
+        isPgc to 9,
+        notReportedAd to 12,
+        isAreaLimited to 13,
+        blockAddTags to 14,
+    ).booleansToInt()
+
+    public companion object {
+        public fun decode(data: Int): VideoAttribute {
+            data.intToBooleans().let {
+                return VideoAttribute(
+                    noRanking = it[0],
+                    noFeed = it[1],
+                    noWeb = it[2],
+                    noApp = it[3],
+                    noSearch = it[4],
+                    noRecommend = it[6],
+                    isPgc = it[9],
+                    notReportedAd = it[12],
+                    isAreaLimited = it[13],
+                    blockAddTags = it[14],
+                )
+            }
+        }
+    }
+}
+
+private fun List<Pair<Boolean, Int>>.booleansToInt(): Int {
+    var result = 0
+    forEach { (key, value) ->
+        if (key) {
+            result = result or (1 shl value)
+        }
+    }
+    return result
+}
+
+private fun Int.intToBooleans(): BooleanArray {
+    val result = BooleanArray(32)
+    for (i in 0..31) {
+        result[i] = (this shr i) and 1 == 1
+    }
+    return result
+}
