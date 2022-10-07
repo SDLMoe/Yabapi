f8d82d0509e29f1f74430efca2dd9654fefeae35
Colerar <233hbj@gmail.com>
2022-10-07 19:47:39 +0800

    build: improve version, release 0.11.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,21 @@
+import java.util.Properties
+
+val local = Properties()
+local.load(project.rootProject.file("local.properties").inputStream().buffered())
+
 allprojects {
     group = "moe.sdl.yabapi"
-    version = "0.11.0"
+    version = if (local["build.snapshot"]?.toString()?.toBoolean() != true) {
+        "0.11.1"
+    } else {
+        val ver = local["build.version"]
+            as? String
+            ?: error("build.version is not String")
+        if (!ver.endsWith("-SNAPSHOT")) {
+            error("Snapshot version does not end with '-SNAPSHOT'")
+        }
+        ver
+    }
     repositories {
         mavenCentral()
     }
